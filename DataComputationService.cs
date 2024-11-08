c5de83bc6a9f9ba8bc180cc03fc9e0249ec5160f
Keshlin Moodley <Keshlin.Moodley@autotrak.co.za>
2024-11-08 13:17:23 +0200

    Add project files.

---
+++ DataComputationService.cs
@@ -0,0 +1,105 @@
+﻿namespace AATTechnical
+{
+    public class DataComputationService
+    {
+        private readonly List<int> globalList = new();
+        private readonly object lockObj = new();
+        private const int maxEntries = 10000000;
+        private bool evenThreadStarted = false;
+
+        public void StartComputation()
+        {
+            Task.Run(() => GenerateOddNumbers());
+            Task.Run(() => GenerateNegatedPrimes());
+        }
+
+        private void GenerateOddNumbers()
+        {
+            while (globalList.Count < maxEntries)
+            {
+                int number = GenerateRandomOdd(0, 1000);
+                AddToGlobalList(number);
+                CheckStartEvenThread();
+            }
+        }
+
+        private void GenerateNegatedPrimes()
+        {
+            int primeCandidate = 2;
+            while (globalList.Count < maxEntries)
+            {
+                if (IsPrime(primeCandidate))
+                {
+                    AddToGlobalList(-primeCandidate);
+                }
+                primeCandidate++;
+            }
+        }
+
+        private void CheckStartEvenThread()
+        {
+            if (globalList.Count >= 2500000 && !evenThreadStarted)
+            {
+                Task.Run(() => GenerateEvenNumbers());
+                evenThreadStarted = true;
+            }
+        }
+
+        private void GenerateEvenNumbers()
+        {
+            while (globalList.Count < maxEntries)
+            {
+                int number = GenerateRandomEven(0, 1000);
+                AddToGlobalList(number);
+            }
+        }
+
+        private void AddToGlobalList(int number)
+        {
+            lock (lockObj)
+            {
+                if (globalList.Count < maxEntries)
+                {
+                    globalList.Add(number);
+                }
+            }
+        }
+
+        private bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        public int GenerateRandomOdd(int min, int max)
+        {
+            Random rand = new Random();
+            int number;
+            do
+            {
+                number = rand.Next(min, max);
+            } while (number % 2 == 0);
+            return number;
+        }
+
+        public int GenerateRandomEven(int min, int max)
+        {
+            Random rand = new Random();
+            int number;
+            do
+            {
+                number = rand.Next(min, max);
+            } while (number % 2 != 0);
+            return number;
+        }
+    }
+
+}
---
+++ DataPersistenceService.cs
@@ -0,0 +1,56 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Serialization;
+using System;
+
+namespace AATTechnical
+{
+    public class DataPersistenceService
+    {
+        private readonly AppDbContext _context;
+
+        public DataPersistenceService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SaveDataAsync(IEnumerable<int> data)
+        {
+            foreach (var value in data)
+            {
+                await _context.Numbers.AddAsync(new Number
+                {
+                    Value = value,
+                    IsPrime = IsPrime(value) ? 1 : 0
+                });
+            }
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<byte[]> GetXmlDataAsync()
+        {
+            var numbers = await _context.Numbers.ToListAsync();
+            var xmlSerializer = new XmlSerializer(typeof(List<Number>));
+            using var memoryStream = new MemoryStream();
+            xmlSerializer.Serialize(memoryStream, numbers);
+            return memoryStream.ToArray();
+        }
+
+        public async Task<byte[]> GetBinaryDataAsync()
+        {
+            var numbers = await _context.Numbers.ToListAsync();
+            using var memoryStream = new MemoryStream();
+            using var binaryWriter = new BinaryWriter(memoryStream);
+
+            foreach (var number in numbers)
+            {
+                binaryWriter.Write(number.Value);
+                binaryWriter.Write(number.IsPrime);
+            }
+
+            return memoryStream.ToArray();
+        }
+
+//        private bool IsPrime(int number) => // Implement prime checking logic
+}
+
+}
